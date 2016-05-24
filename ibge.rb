966279fd6f53f43e93a3e0710742a37d750c083c
sander <sanderlacerda@users.noreply.github.com>
2016-05-24 14:27:40 -0300

    Add files via upload

--- ibge.rb
+++ ibge.rb
@@ -35,3 +35,38 @@ def modalidade(b,modalidade)
 	return b
 end
 
+# --------------------------------------------------------------------------------
+#                               funções específicas para cada série
+# --------------------------------------------------------------------------------
+
+def pme()
+	
+	sites = [
+		["http://www.sidra.ibge.gov.br/bda/tabela/listabl.asp?c=2181&z=t&o=16","_real_do_trabalho"],
+		["http://www.sidra.ibge.gov.br/bda/tabela/listabl.asp?c=2189&z=t&o=16","_nominal_do_trabalho"]
+	]
+	
+	b = Selenium::WebDriver.for :firefox
+	
+	(0..sites.size-1).each do |i|
+		
+		b.navigate.to sites[i][0]
+		# variável
+		b = selecao(b,'opv','Tudo')	
+		# datas
+		b = selecao(b,'opp','Tudo')	
+		b = selecao(b,'pop','Na linha')	
+		# regiões metropolitanas
+		b = selecao(b,'opn7','Tudo')	
+		# gravar			
+		b = gravar(b)
+		# nome de arquivo
+		nome = 'pme_rendimento' + sites[i][1]
+		b = nome_de_arquivo(b,nome)
+		# modalidade
+		b = modalidade(b,"envie") #A posteriori ( #notifique
+		# ok
+		b.find_element(:name, "frmOpcao").submit
+	end
+end
+
